1728fa362e76a258034125a0fa16db314a8b16f8
Bruno Souza <mr.brunosouza@gmail.com>
2021-07-13 18:57:56 -0300

    criada sessão sobre nós

--- src/Components/AboutUs/index.tsx
+++ src/Components/AboutUs/index.tsx
@@ -1,16 +1,55 @@
-import { Container } from "./styles";
+import { useState } from "react";
+import { Buttons, Container } from "./styles";
+
+const whyChooseUs = [
+  {
+    id: 1,
+    title: 'Segurança acima de tudo',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facere neque expedita, ipsum cupiditate iusto ut delectus laudantium, impedit aliquam voluptatem ullam, alias optio eius incidunt cumque. Eaque, amet enim!'
+  },
+  {
+    id: 2,
+    title: 'Compromisso com a entrega',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam facere neque expedita, ipsum cupiditate iusto ut delectus laudantium, impedit aliquam voluptatem ullam, alias optio eius incidunt cumque. Eaque, amet enim!'
+  },
+]
 
 export function AboutUs() {
+  const [optionSelected, setOptionSelected] = useState<number>();
+  const [active, setActive] = useState(false)
 
   return (
     <Container>
       <section className="section-about">
         <h2>Por que escolher a LC Reformas?</h2>
+        {whyChooseUs.map((option) => (
+          <Buttons key={option.id}>
+            <button 
+              onClick={() => {setOptionSelected(option.id), setActive(!active)}} 
+              className={`${option.id === optionSelected ? 'active' : ''}`}
+            >
+              {option.id === optionSelected ? <img src="not-show.svg" alt="" /> : <img src="show-more.svg" alt="" />}
+              {option.title}
+              <svg width="117" height="63" viewBox="0 0 117 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path 
+                  d="M114.9 1.2C113.3 -0.4 110.7 -0.4 109.1 1.2L58.1 52.3L7 1.2C5.4 
+                  -0.4 2.8 -0.4 1.2 1.2C-0.4 2.8 -0.4 5.4 1.2 7L55.1 60.9C55.9 61.7 
+                  56.9 62.1 58 62.1C59 62.1 60.1 61.7 60.9 60.9L114.8 7C116.5 5.4 
+                  116.5 2.8 114.9 1.2V1.2Z" 
+                />
+              </svg>
+            </button>
+            <p 
+              className={`${option.id === optionSelected ? 'show-description' : 'description'}`}
+            >
+              {option.description}
+            </p>
+          </Buttons>
+        ))}
       </section>
       <section className="section-about">
         <h2>Sobre nós</h2>
-        <br />
-        <p>
+        <p className="first-paragraph">
           A Obras e reformas LC é uma empresa com mais de 7 anos de mercado e que vem garantindo a satisfação de inúmeros clientes.
         </p>
         <br />
--- src/Components/AboutUs/styles.ts
+++ src/Components/AboutUs/styles.ts
@@ -2,18 +2,79 @@ import styled from "styled-components";
 
 export const Container = styled.section`
  display: flex;
- align-items: center;
+ align-items: flex-start;
  justify-content: space-between;
  gap: 2rem;
 
  padding: 5rem 20rem;
+
  .section-about {
-  width: 100%;
-  
+  width: 100%; 
 
    h2 {
     font-weight: 500;
     text-transform: uppercase;
+    margin-bottom: 2rem;
+   }
+
+   p {
+     font-size: 1.1rem;
+   }
+
+   .first-paragraph {
+     margin-top: 2rem;
    }
  }
+`
+
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  
+
+  &+div {
+    margin-top: 0.5rem;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    border: none;
+    background: #eee;
+    padding: 1rem 1.2rem;
+
+    img {
+      width: 0.9rem;
+      height: 0.9rem;
+    }
+
+    svg {
+      width: 0.7rem;
+      height: 0.7rem;
+      fill: #000;
+    }
+
+    transition: all 0.3s;
+  }
+
+  .active {
+    background: #003E98;
+    color: #FFF;
+
+    svg {
+      fill: #FFF;
+    }
+  }
+
+  .description {
+    display: none;
+  }
+
+  .show-description {
+    padding: 1.8rem 0.5rem;
+  }
 `
